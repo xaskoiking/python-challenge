6857ae3cd1850e5c2919e35edab80371b303e731
xaskoiking <33464972+xaskoiking@users.noreply.github.com>
2018-06-15 18:51:31 -0400

    pyBank Pseudocde
    The file contains the an higher level pseudocode.

---
+++ PyBank/main.py
@@ -0,0 +1,24 @@
+#PROJECT        : ------  Analyzing the financial records of your company -------
+#INPUT/DATA     : ------  CSV file "budget_data.csv"
+#WHAT THE ASK?  : ------  Analyse the csv file, and determine the following
+#-------------------------------------------------------------------------------------------------
+            ''' A) The total number of months included in the dataset
+                B) The total net amount of "Profit/Losses" over the entire period
+                C) The average change in "Profit/Losses" between months over the entire period
+                D) The greatest increase in profits (date and amount) over the entire period
+                E) The greatest decrease in losses (date and amount) over the entire period'''
+#-------------------------------------------------------------------------------------------------
+
+
+
+#Step 1: Import the necessary functions
+
+#Step 2: Declare the necessary variables
+
+#Step 3: Import the csv file
+
+#Step 4: Exclude the Header variable
+
+#Step 5: Iterate the .csv file across each rows and perform the following functions
+
+#Step 6: Print the output in the required format
